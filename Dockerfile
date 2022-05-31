45e4a2f21f2afe90d5b38962a3903be057996771
abdelbasset <abdelbassetwaraki@gmail.com>
2022-05-31 18:07:05 +0100

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:alpine
+WORKDIR /usr/src/app
+COPY package.json .
+COPY yarn.lock .
+RUN yarn install
+COPY . .
+CMD [ "yarn", "start" ]
---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxyMiddleware = createProxyMiddleware({
+  target: process.env.FIGMENT_URL,
+  changeOrigin: true,
+  headers: {
+    Authorization: process.env.FIGMENT_TOKEN,
+  },
+});
+const app = express();
+app.use('/', proxyMiddleware);
+app.listen(process.env.PORT || 8088);
